7e2e2cd8e9ec39792922dedc155ca7642b9580b2
Danila <scriptrixx44@gmail.com>
2025-06-21 08:37:52 +0300

    bug fix (del. postID), add analyze

--- internal/app/interfaces.go
+++ internal/app/interfaces.go
@@ -7,17 +7,20 @@ import (
 	"github.com/ScrpTrx-Go/myfirstproject/internal/model"
 )
 
-// PostFetcher отвечает за получение постов по диапазону дат
+// PostFetcher выдает сообщения по диапазону дат
 type PostFetcher interface {
-	Fetch(ctx context.Context, from, to time.Time, outputChan chan model.Post) error
+	Fetch(ctx context.Context, from, to time.Time) <-chan model.Post
 }
 
-// Analyzer обрабатывает список постов и возвращает статистику
+// Analyzer принимает контекст, канал с постами, выполняет логику с постом, возвращает обновленный model.Post, который передается в SaveToDB
 type Analyzer interface {
-	Analyze(posts []model.Post) Stats
+	Analyze(ctx context.Context, postFromFetch <-chan model.Post) <-chan model.Post
 }
 
 // ReportGenerator создает документ с постами и статистикой
 type ReportGenerator interface {
 	Generate(posts []model.Post, stats Stats) error
 }
+
+/* var ch1 chan<- int  // только запись (send-only)
+var ch2 <-chan int  // только чтение (receive-only) */
